f380387707cb7c886e71c4a1b0eb4f8d81de7d65
Alexandra Khalimon <lexikhalimon@gmail.com>
2025-06-21 22:44:54 +0300

    update files

--- src/main.js
+++ src/main.js
@@ -5,36 +5,38 @@ import "izitoast/dist/css/iziToast.min.css";
 
 const searchForm = document.querySelector(".form");
 const searchInput = document.querySelector("input");
-const searchBtn = document.querySelector("button");
 const galleryList = document.querySelector(".gallery");
+
 
 searchForm.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
     event.preventDefault();
+
     const query = searchInput.value.trim();
 
     showLoader();
     clearGallery();
 
-    getImagesByQuery(query)
-        .then(response => {
-            if (response.hits.length === 0) {
-                return iziToast.show({
-                    message: "Sorry, there are no images matching your search query. Please try again!",
-                    color: "red",
-                    position: "topRight"
-                })
-            }
-            galleryList.innerHTML = (createGallery(response.hits));
-            lightboxModal();
-            hideLoader();
-            
-        })
-        .catch(error => {
-            console.log(error);
-        })
-        .finally(() => {
-            hideLoader();
-        });
+    setTimeout(() => {
+        getImagesByQuery(query)
+            .then(response => {
+                if (response.hits.length === 0) {
+                    return iziToast.show({
+                        message: "Sorry, there are no images matching your search query. Please try again!",
+                        color: "red",
+                        position: "topRight"
+                    })
+                };
+
+                galleryList.innerHTML = (createGallery(response.hits));
+                lightboxModal();
+            })
+            .catch(error => {
+                console.log(error.message);
+            })
+            .finally(() => {
+                hideLoader();
+            });
+    }, 1000);
 }
